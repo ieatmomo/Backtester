c11ffe9a80ed97430f887698ecac2e6555923957
ieatmomo <deeveshrai125@gmail.com>
2026-02-06 06:33:13 +0000

    Working CLI and first run

--- cli.py
+++ cli.py
@@ -1,10 +1,26 @@
 import argparse
+from engine.loop import Loop 
+import pandas as pd
 
+
+#data\AAPL_2024-01-03T00%3A00%3A00Z_2024-01-04T00%3A00%3A00Z.csv -> Test File Path
 parser = argparse.ArgumentParser()
 parser.add_argument("data_path")
-parser.add_argument("strategy")
+# parser.add_argument("strategy")
+parser.add_argument("starting_cash")
 
 args = parser.parse_args()
 
 data_path = args.data_path
-strategy = args.strategy
+starting_cash = float(args.starting_cash)
+
+#Currently for V1 default to moving_averages, need to refactor code for this to be input
+# strategy = args.strategy
+
+engine_loop = Loop(data_path, starting_cash)
+
+engine_loop.iterate_bars()
+
+df = pd.DataFrame(engine_loop.trade_record_store)
+
+df.to_csv('TEST_TRADE_RECORDS.csv')
--- engine/loop.py
+++ engine/loop.py
@@ -2,25 +2,28 @@
 Orchestrates
 '''
 from strategies.moving_averages import SMA 
-from execution import ExecutionV1
-from portfolio import Portfolio
+from engine.execution import ExecutionV1
+from engine.portfolio import Portfolio
 import csv
 import pandas as pd
 #TAKES ROW OF DATA AND PARSES IT -> SEND TO EXECUTION
 #ADD EQUITY CURVES WHEN NEEDED :3
 
+#TODO: Takes variables from CLI -> Cash, File path, 
+
 class Loop():
-    def __init__(self):
+    def __init__(self, file_path, starting_cash):
+        self.file_path = file_path
         self.strategy = SMA()
-        self.portfolio = Portfolio(1000) #AT THIS POINT JUST INITIALISE CASH AS 1000, CAN CHANGE LATER FROM CLI INPUT
+        self.portfolio = Portfolio(starting_cash) #AT THIS POINT JUST INITIALISE CASH AS 1000, CAN CHANGE LATER FROM CLI INPUT
         self.execution = ExecutionV1(0, 0, self.portfolio) #FEES AND SLIPPAGE AT 0 FOR V1
         self.pending_signal = "NO_CHANGE"
         self.trade_record_store = []
 
-    def iterate_bars(self, file_path):
+    def iterate_bars(self):
         #data\AAPL_2024-01-03T00%3A00%3A00Z_2024-01-04T00%3A00%3A00Z.csv
         #header = timestamp,open,high,low,close,volume,volume_weighted_average_price,number_of_trades
-        with open(file_path) as data:
+        with open(self.file_path) as data:
 
             reader_obj = csv.reader(data)
             next(reader_obj)
